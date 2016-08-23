13cfdef1ae2af856d5636c3450397fcca4db33b8
David Speare <speare.david@gmail.com>
2016-08-22 21:03:44 -0400

    Revert "Revert "Messed up the robot prefab""
    This reverts commit ff586e844ea0c93d59e9fd5e97b49d9367f085fa.

--- Source/Assets/Single Player/TinyBots/IsPlayerDead.cs
+++ Source/Assets/Single Player/TinyBots/IsPlayerDead.cs
@@ -4,62 +4,8 @@ using System.Collections;
 public class IsPlayerDead : MonoBehaviour {
 
 
-
+    //set in BreakJointsOnGoodHit
 	public bool dead = false;
 
-	SpriteRenderer[] myRenderers;
-	Transform[] myGameObjects;
 
-	bool spawnInvulnerable = true;
-
-	//int bossImmune = 12;
-	int normal = 8;
-    /*
-	IEnumerator Flash(){
-		while (spawnInvulnerable) {
-			foreach (SpriteRenderer r in myRenderers) {
-				if (r.color.a > 0.9) {
-					Color boobs = r.color;
-					boobs.a = 0.4f;
-					r.color = boobs;
-				} else {
-					Color boobs = r.color;
-					boobs.a = 1f;
-					r.color = boobs;
-				}
-			}
-			yield return new WaitForSeconds (0.2f);
-		}
-		foreach (SpriteRenderer r in myRenderers) {
-			Color boobs = r.color;
-			boobs.a = 1f;
-			r.color = boobs;
-		}
-	}
-
-	IEnumerator BeInvinclible(){
-		spawnInvulnerable = true;
-		yield return new WaitForSeconds (2);
-		spawnInvulnerable = false;
-		foreach (Transform t in myGameObjects) {
-			t.gameObject.layer = normal;
-		}
-	}
-
-	void Update(){
-		//print (gameObject.layer);
-	}
-
-	void Start(){
-		myRenderers = GetComponentsInChildren<SpriteRenderer> ();
-		myGameObjects = GetComponentsInChildren<Transform> ();
-
-		foreach (Transform t in myGameObjects) {
-			t.gameObject.layer = bossImmune;
-		}
-
-		StartCoroutine (BeInvinclible ());
-		StartCoroutine (Flash ());
-	}
-    */
 }
